782381e9306750054f7a6d86a037e1445aecbc6f
Shnataa <211210036@students.mercubuana-yogya.ac.id>
2025-05-28 00:48:35 +0700

    update siza

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
     <li class="nav-item nav-link">
         <div class="d-flex align-items-center mx-3 mt-3">
             <!-- Add your image here -->
-            <img src="{{ asset('img/logo.png') }}" alt="Logo" style="width: 30px; height: 30px; margin-right: 10px;">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo" style="width: 50px; height: 50px; margin-right: 10px;">
             <!-- Title text with custom class for responsive font size -->
             <div class="sidebar-title" style="color: #0508B4; font-weight:bold;">SPK DPU Bangka Barat</div>
         </div>
@@ -94,7 +94,7 @@
     <li class="nav-item nav-link">
         <div class="d-flex align-items-center mx-3 mt-3">
             <!-- Add your image here -->
-            <img src="{{ asset('img/logo.png') }}" alt="Logo" style="width: 30px; height: 30px; margin-right: 10px;">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo" style="width: 50px; height: 50px; margin-right: 10px;">
             <!-- Title text with custom class for responsive font size -->
             <div class="sidebar-title" style="color: #0508B4; font-weight:bold;">SPK DPU Bangka Barat</div>
         </div>
